78def5486549f945a7aa6d6e0c36f4242b47d34d
DESKTOP-QIDODUT\acer <snowman12320@gmail.com>
2022-07-10 15:04:38 +0800

    更新，安裝含有route的

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,25 @@
-// which is lazy-loaded when the route is visited.
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    },
-    {
-        path: '/newpage',
-        name: '新增頁面',
-        component: () => import('../views/NewPage.vue'),
-    },
-];
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-});
+  history: createWebHashHistory(),
+  routes
+})
 
-export default router;
+export default router
