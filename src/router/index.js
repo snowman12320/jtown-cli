b053017e141969dfc6bb17e846dac8390519a174
william <101874444+snowman12320@users.noreply.github.com>
2022-09-07 14:45:35 +0800

    確認是否維持登入狀態

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,10 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/dashboard',
+    component: () => import('../views/Dashboard.vue')
   }
 ];
 
