efb3028450988a5ead5e27ca8f6c3cb945050203
DESKTOP-QIDODUT\acer <snowman12320@gmail.com>
2022-09-07 21:57:21 +0800

    增加 Bootstrap Modal

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,13 @@
 <template>
-  產品列表
+  <div class="text-end mt-3">
+    <button
+      class="btn btn-primary"
+      type="button"
+      @click="$refs.productModal.showModal()"
+    >
+      增加一個產品
+    </button>
+  </div>
   <table class="table mt-4">
     <thead>
       <tr>
@@ -34,15 +42,21 @@
       </tr>
     </tbody>
   </table>
+  <ProductModal ref="productModal"></ProductModal>
 </template>
 
 <script>
+import ProductModal from '../components/ProductModal.vue';
+
 export default {
   data () {
     return {
       products: [],
       pagination: {}
     };
+  },
+  components: {
+    ProductModal
   },
   methods: {
     getProducts () {
