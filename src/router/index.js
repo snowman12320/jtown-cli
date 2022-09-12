7374c784a9a37c35555ad0fbfb92888581b3d08c
william <101874444+snowman12320@users.noreply.github.com>
2022-09-12 15:42:07 +0800

    加入錯誤的訊息回饋

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+// import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    // component: HomeView
+    // name: 'login',
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/about',
