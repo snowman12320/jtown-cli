47e90fed9ca15216b69d91d25f19733b5fcd8135
snowman12320 <snowman12320@gmail.com>
2023-07-13 22:01:19 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/hello-vue-app/',
+  publicPath: '/hello-vue-app/'
 });
