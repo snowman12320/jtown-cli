a77dcc080ce1774af33378c3f65376658b9a5dfe
william <snowman12320@gmail.com>
2023-08-18 10:33:30 +0800

    present homework

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   // publicPath: './' //! 會錯誤路徑
-  publicPath: process.env.NODE_ENV === 'production' ? '/hexschool-question/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/jtown-cli/' : '/'
 });
 //
 // 熱重載失效，找不到解法，故先註解
-=======
->>>>>>> 9095ef7c3885d9385198273f6a3c5d244f788c86
 // const { defineConfig } = require('@vue/cli-service');
 // module.exports = defineConfig({
 //   transpileDependencies: true,
-//   publicPath: process.env.NODE_ENV === 'production' ? '/hexschool-question/' : '/'
+//   publicPath: process.env.NODE_ENV === 'production' ? '/jtown-cli/' : '/'
 // });
 //
 const { defineConfig } = require('@vue/cli-service');
